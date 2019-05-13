a91a42e8cf3abbd6a739bfe41962f407f825cdb5
qiqetes <qiqete@gmail.com>
2019-05-13 18:56:12 +0200

    modified some fxml and added css style

--- src/crossfitsessionmanager/FXMLManageGroupsController.java
+++ src/crossfitsessionmanager/FXMLManageGroupsController.java
@@ -9,6 +9,7 @@ import accesoBD.AccesoBD;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import modelo.Grupo;
@@ -23,14 +24,19 @@ public class FXMLManageGroupsController implements Initializable {
     
     
     
-    AccesoBD accesoBD;
-    public static ObservableList<Grupo> groupObservableList;
+    AccesoBD singleton;
+    public static ObservableList<Grupo> groupObsList;
     ArrayList<Grupo> groupData = new ArrayList<Grupo>();
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        accesoBD  = AccesoBD.getInstance();
+        singleton  = AccesoBD.getInstance();
+        groupData = singleton.getGym().getGrupos();
+        groupObsList = FXCollections.observableArrayList(groupData);
+        listViewGroups.setItems(groupObsList);
         
+        /*Cell Factory for templates listView*/
+        listViewSessions.setCellFactory(c -> new SessionTemplateListCell());
     }    
     
     static void initStage() {
