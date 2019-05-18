4144a6e07384bac1b9f10e47b2ca1b7913481714
Borja <borjasanz11@hotmail.com>
2019-05-18 13:46:13 +0200

    Chrono started

--- src/crossfitsessionmanager/FXMLIntervalTimerController.java
+++ src/crossfitsessionmanager/FXMLIntervalTimerController.java
@@ -6,13 +6,28 @@
 package crossfitsessionmanager;
 
 import java.net.URL;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableBooleanValue;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import modelo.Grupo;
 import modelo.SesionTipo;
 
@@ -23,25 +38,79 @@ import modelo.SesionTipo;
  */
 public class FXMLIntervalTimerController implements Initializable {
 
+    @FXML
+    private Text lDisplayedMsg;
+    @FXML
+    private Text lTime;
+    
     private SesionTipo template;
     private Grupo group;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
     
+    private MyCronoTask task;
+        
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        startTime = LocalDateTime.now();
+        task = new MyCronoTask();
+        task.setTimeCrono(30);
+        
+        Thread hilo = new Thread(task);
+        hilo.setDaemon(true);
+        hilo.start();
+        
+        lTime.textProperty().bind(task.messageProperty());
     }    
     
     public void initStage(SesionTipo sT, Grupo g) {
         template = sT;
-        System.out.println(template.getCodigo());
         group = g;
-        System.out.println(group);
     }
 
     @FXML
-    private void onClickPlay(MouseEvent event) {
+    private void onClickPlay(ActionEvent event) {
+        task.startTimer();
+    }
+
+    class MyCronoTask extends Task<Void>{
+        boolean started = false;
+        long secTotal;
+        long secActual;
+        long startTime;
+        
+        void startTimer(){
+            started = true;
+            startTime = System.currentTimeMillis();
+        }
+        
+        void setTimeCrono(int sec){
+            secTotal = sec;
+            secActual = secTotal;
+        }
+        
+        void calcula() {
+            secActual = secTotal - (System.currentTimeMillis() - startTime)/1000;
+            updateMessage("" + secActual);
+        }
+        
+        @Override
+        protected Void call() {            
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    if (isCancelled()) {
+                        break;
+                    }
+                }
+                if (isCancelled()) {
+                    break;
+                }
+                while(started){
+                    calcula();
+                }
+            }
+            return null;
+        }
     }
 }
+
